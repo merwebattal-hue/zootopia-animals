6d8573a6bb0170943b8da8bc956409a1de2277e6
Merve Battal <merwe.battal@gmail.com>
2026-01-30 15:53:35 +0100

    Render HTML using Jinja2 template

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,19 +1,27 @@
 import json
 
+
 def load_data(file_path):
     """Loads a JSON file"""
-    with open(file_path, "r") as handle:
+    with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
+
 
 animals_data = load_data("animals_data.json")
 
 for animal in animals_data:
     if "name" in animal:
         print(f"Name: {animal['name']}")
-    if "diet" in animal:
-        print(f"Diet: {animal['diet']}")
+
+    characteristics = animal.get("characteristics", {})
+
+    if "diet" in characteristics:
+        print(f"Diet: {characteristics['diet']}")
+
     if "locations" in animal and animal["locations"]:
         print(f"Location: {animal['locations'][0]}")
-    if "type" in animal:
-        print(f"Type: {animal['type']}")
+
+    if "type" in characteristics:
+        print(f"Type: {characteristics['type']}")
+
     print()
